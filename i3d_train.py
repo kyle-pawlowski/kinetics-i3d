0984ae37a6250c485a6bd8c49c120407dc6a4d99
kyle-pawlowski <kylepawlowski1@gmail.com>
2020-06-16 22:31:08 -0400

    get network to produce logs and checkpoints

--- i3d_train.py
+++ i3d_train.py
@@ -11,6 +11,7 @@ import sonnet as snt
 import numpy as np
 from i3d import InceptionI3d
 from UCF_utils import sequence_generator, get_data_list
+import datetime
 
 #tf.compat.v1.enable_eager_execution()
 num_classes = 101
@@ -45,7 +46,8 @@ def train_step(net, example, optimizer):
   optimizer.apply_gradients(zip(gradients, variables))
   return loss
 
-def session_train(optimizer):
+def session_train(optimizer,epochs):
+    #build network with fake inputs
     flow_input = tf.placeholder(tf.float32,shape=tf.TensorShape([10,12,216,216,4]))
     flow_answers = tf.placeholder(tf.float32,shape=(10,101))
     with tf.variable_scope('Flow'):
@@ -53,24 +55,52 @@ def session_train(optimizer):
         logits, _ = i3d(flow_input,is_training=True)
     predictions = tf.nn.softmax(logits)
     loss = tf.reduce_mean(tf.abs(predictions - flow_answers))
-        
+    
+    #create data iterator
     data = data_gen()
     iterator = tf.compat.v1.data.make_one_shot_iterator(data)
     datax,datay = iterator.get_next()
     datax=tf.cast(datax,tf.float32)
     
-    global_step = tf.Variable(0,tf.int64)
-    training_opt = optimizer.minimize(loss,global_step=global_step)
+    #create folders for summary logs
+    # https://www.tensorflow.org/tensorboard/get_started
+    time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    train_log_dir = 'logs/training' + time
+    train_summary_writer = tf.compat.v1.summary.FileWriter(train_log_dir)
+    
+    #create variable map to save like in evaluate sample
+    variable_map = {}
+    for variable in tf.global_variables():
+      if variable.name.split('/')[0] == 'Flow':
+        variable_map[variable.name.replace(':0', '')] = variable
+    
+    #create checkpoint 
+    # https://www.tensorflow.org/versions/r1.15/api_docs/python/tf/train/Checkpoint
+    #savior = tf.train.Saver(var_list=variable_map, reshape=True)
+    #ckpt = tf.train.Checkpoint(optimizer=optimizer, model=i3d)
+    #manager = tf.train.CheckpointManager(ckpt, './tf_ckpts', max_to_keep=3)
+    
+    #evaluation
+    global_step = tf.compat.v1.train.get_or_create_global_step()
+    step_counter_hook = tf.estimator.StepCounterHook(summary_writer=train_summary_writer)
+    training_opt = optimizer.minimize(loss,global_step=tf.compat.v1.train.get_global_step())
     is_chief = True
     sync_replicas_hook = optimizer.make_session_run_hook(is_chief)
-    with tf.compat.v1.train.MonitoredTrainingSession(is_chief=is_chief,hooks=[sync_replicas_hook]) as sess:
-        while not sess.should_stop():
+    step_counter_hook.begin()
+    with tf.compat.v1.train.MonitoredTrainingSession(is_chief=is_chief,
+                                                     hooks=[sync_replicas_hook,step_counter_hook],
+                                                     checkpoint_dir='./tf_ckpts',
+                                                     summary_dir='./logs',
+                                                     save_summaries_steps=1,
+                                                    save_checkpoint_steps=2) as sess:
+        for epoch in range(epochs):
             feed_dict = {}
             feed_dict[flow_input] = sess.run(datax)
             feed_dict[flow_answers] = sess.run(datay)
-            result = sess.run(training_opt,feed_dict=feed_dict)
-    
-        
+            sess.run(training_opt,feed_dict=feed_dict)
+            #ckpt.step.assign_add(1)
+            #if epoch % 2 == 0:
+             # save_path = savior.save(sess, 'my_model', global_step=global_step)
     
 def train_network():
     #i3d = InceptionI3d(num_classes=num_classes,spatial_squeeze=False,final_endpoint='Predictions')
@@ -81,7 +111,7 @@ def train_network():
     '''for example in iter(data_gen()):
         print("loop")
         train_step(i3d,example,opt)'''
-    session_train(opt)
+    session_train(opt,2)
     
 if __name__ is "__main__":
     train_network()

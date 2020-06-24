2fa9084ca20a900e52cee301c7c2e6727d264ff3
kyle-pawlowski <kylepawlowski1@gmail.com>
2020-06-24 19:11:18 -0400

    added back normalization because it broke dmd

--- dmd_preprocessing.py
+++ dmd_preprocessing.py
@@ -102,14 +102,14 @@ def _compute_dmd(frames, svd_rank):
     dmd = DMD(svd_rank=svd_rank)
     #print("input is nan: " + str(np.isnan(vec_frames).any()))
     #print("input is inf: " + str(np.isinf(vec_frames).any()))
-    #vec_frames /= 255.
+    vec_frames /= 255.
     vec_frames = np.where(vec_frames==0,1,vec_frames)
     dmd.fit(np.nan_to_num(vec_frames.T,posinf=255,neginf=1))
     modes = dmd.modes.real
     return modes
  
 if __name__ == '__main__':
-    sequence_length = 10 
+    sequence_length = 16 
     image_size = (216,216,3)
     cwd = os.getcwd()
     src_dir = os.path.join(cwd,'data/UCF-Preprocessed-DMD')
